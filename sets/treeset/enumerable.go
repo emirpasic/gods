213367f1ca932600ce530ae11c8a8cc444e3a6da
Emir Pasic <emirpasic@hotmail.com>
2016-06-27 04:21:09 +0200

    - replace long bsd text with short in all .go files

--- sets/treeset/enumerable.go
+++ sets/treeset/enumerable.go
@@ -1,20 +1,6 @@
-/*
-Copyright (c) Emir Pasic, All rights reserved.
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 3.0 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library. See the file LICENSE included
-with this distribution for more information.
-*/
+// Copyright (c) 2015, Emir Pasic. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package treeset
 
